fab75a3600afb028f4ecfc86a7f320dfbeffe623
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-06-16 22:50:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
   if (branchItemTab) showSection("mbs", branchItemTab);
   if (buyItemTab) showSection("tore", buyItemTab);
 });
+
+function filterUsers() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const table = document.getElementById("usersTable");
+  const tr = table.getElementsByTagName("tr");
+
+  let visibleCount = 0;
+
+  for (let i = 1; i < tr.length; i++) {
+    const td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      const txtValue = td.textContent || td.innerText;
+      const match = txtValue.toLowerCase().includes(input);
+      tr[i].style.display = match && visibleCount < 10 ? "" : "none";
+      if (match) visibleCount++;
+    }
+  }
+}

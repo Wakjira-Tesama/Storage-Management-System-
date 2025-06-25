5c72241407251c34d2dcb6f8f7d73ed7a2f0f536
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-06-25 21:03:28 +0300

    Update storeManager.php

--- storeManager.php
+++ storeManager.php
@@ -27,3 +27,28 @@ if (isset($_POST['add_item'])) {
     $manager = $_SESSION['username'];
     $conn->query("INSERT INTO activity_log (username, activity) VALUES ('$manager', 'Added item $name')");
 }
+
+if (isset($_POST['delete_item'])) {
+    $id = $_POST['item_id'];
+    $conn->query("DELETE FROM inventory WHERE id=$id");
+    $conn->query("INSERT INTO activity_log (username, activity) VALUES ('$manager', 'Deleted item ID $id')");
+}
+if (isset($_POST['update_item'])) {
+    $id = $_POST['item_id'];
+    $qty = $_POST['quantity'];
+    $price = $_POST['unit_price'];
+
+    if (!filter_var($qty, FILTER_VALIDATE_INT, ["options" => ["min_range" => 1]])) {
+        die("Quantity must be a positive integer.");
+    }
+
+    $stmt = $conn->prepare("UPDATE inventory SET quantity=?, unit_price=? WHERE id=?");
+    $stmt->bind_param("idi", $qty, $price, $id);
+    $stmt->execute();
+
+    $conn->query("INSERT INTO activity_log (username, activity) VALUES ('$manager', 'Updated quantity and price for item ID $id')");
+}
+
+if (isset($_POST['approve_request'])) {
+    $id = $_POST['request_id'];
+
